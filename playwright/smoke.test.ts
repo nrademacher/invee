1cd8beacf30d0fe0361bb34e223bc01960a32839
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-02-15 01:29:02 +0100

    chore: fix smoke test

--- playwright/smoke.test.ts
+++ playwright/smoke.test.ts
@@ -3,7 +3,7 @@ jest.setTimeout(35e3)
 test('go to /', async () => {
     await page.goto('http://localhost:3000')
 
-    await page.waitForSelector(`text=Starter`)
+    await page.waitForSelector(`text=invee`)
 })
 
 test('go to post', async () => {
@@ -13,7 +13,7 @@ test('go to post', async () => {
 
     await page.click('text=Sign Up')
 
-    expect(page.locator(`text=Signup`)).toBeDefined()
+    expect(page.locator(`text=Sign Up`)).toBeDefined()
 })
 
 export {}
